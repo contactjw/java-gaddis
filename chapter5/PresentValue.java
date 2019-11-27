c1a32c784405a82b6cdeedb2ea04e851a9ae653c
John West <contactjohnwest@gmail.com>
2019-11-26 23:11:12 -0800

    Finished Chapter 5

--- chapter5/PresentValue.java
+++ chapter5/PresentValue.java
@@ -1,5 +1,41 @@
 package chapter5;
+import java.util.Scanner;
 
 public class PresentValue {
-
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double presentVal,
+			   futureVal,
+			   annualInterestRate,
+			   years;
+		String keepGoing = "yes";
+		
+		while (keepGoing.compareToIgnoreCase("yes") == 0) {
+			System.out.print("Enter the future value you wish to obtain from savings: $");
+			futureVal = keyboard.nextDouble();
+			
+			System.out.print("Enter the annual interest rate of your savings account (5% = 0.05): ");
+			annualInterestRate = keyboard.nextDouble();
+			
+			System.out.print("Enter the number of years you want this amount by: ");
+			years = keyboard.nextDouble();
+			
+			presentVal = presentValue(futureVal, annualInterestRate, years);
+			System.out.printf("You need to deposit $%,.2f today to get $%,.2f in %.0f years.\n",
+							presentVal, futureVal, years);
+			
+			keyboard.nextLine();
+			
+			System.out.print("\nWould you like to test again (y = yes, n = no): ");
+			keepGoing = keyboard.nextLine();
+			System.out.println();
+			
+		}
+		
+		keyboard.close();
+	}
+	
+	public static double presentValue(double futureVal, double annualInterest, double numYears) {
+		return futureVal / Math.pow((1 + annualInterest), numYears);
+	}
 }
---
+++ chapter5/RockPaperScissorsGame.java
@@ -0,0 +1,89 @@
+package chapter5;
+import java.util.Scanner;
+import java.util.Random;
+
+public class RockPaperScissorsGame {
+	public static void main(String[] args) {
+		int computer,
+			player;
+		
+		do {
+		computer = computerChoice();
+		player = playerChoice();
+		System.out.println("\nComputer chose " + selected(computer));
+		System.out.println("You chose " + selected(player));
+		System.out.println();
+		whoWon(computer, player);
+		} while (player == computer);
+		
+	}
+	
+	public static int computerChoice() {
+		Random randomNum = new Random();
+		int computerChoice = randomNum.nextInt(3) + 1;
+		return computerChoice;
+	}
+	
+	public static int playerChoice() {
+		// Ignore this warning JVM handles closing scanners.
+		@SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
+		int playerChoice;
+		menu();
+		System.out.print("Enter your choice: ");
+		playerChoice = keyboard.nextInt();
+		while (playerChoice < 1 || playerChoice > 4) {
+			System.out.print("Enter one of the choices displayed: ");
+			playerChoice = keyboard.nextInt();
+		}
+		if (playerChoice == 4)
+			System.exit(0);
+		return playerChoice;
+	}
+	
+	public static String selected(int value) {
+		String choice = "";
+		switch (value) {
+			case 1:
+				choice = "Rock";
+				break;
+			case 2:
+				choice = "Paper";
+				break;
+			case 3:
+				choice = "Scissors";
+				break;
+		}
+		return choice;
+	}
+	
+	public static void whoWon(int computer, int player) {
+		if (computer == 1 && player == 3)
+			System.out.println("Computer wins!");
+		else if (computer == 1 && player == 2)
+			System.out.println("You win!");
+		else if (computer == 1 && player == 1)
+			System.out.println("You tied.");
+		else if (computer == 2 && player == 1)
+			System.out.println("Computer wins!");
+		else if (computer == 2 && player == 2)
+			System.out.println("You tied!");
+		else if (computer == 2 && player == 3)
+			System.out.println("You win!");
+		else if (computer == 3 && player == 1)
+			System.out.println("You win!");
+		else if (computer == 3 && player == 2)
+			System.out.println("Computer wins!");
+		else
+			System.out.println("You tied.");
+	}
+	
+	public static void menu() {
+		System.out.println("Menu Selection");
+		System.out.println("1. Rock");
+		System.out.println("2. Paper");
+		System.out.println("3. Scissors");
+		System.out.println("4. Quit");
+	}	
+	
+}
